d503770d42d093c298eb003c9e7bcdaec0e90ec4
Sirisha3010 <sirisha30102002@gmail.com>
2025-08-05 12:58:38 +0530

    done

---
+++ src/main/java/pack2/Lab9.java
@@ -0,0 +1,23 @@
+package pack2;
+import java.util.Scanner;
+
+public class Lab9 {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter a num:");
+			int num= sc.nextInt();
+			if(num > 0) {
+				System.out.println("num is positive  num");
+				
+			}
+			else {
+				System.out.println("num is negative  num");
+			}
+			
+
+		}
+
+	}
+
+
